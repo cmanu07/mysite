88d54c7ac3c3b7e1eaa60bbdb0c42420263265d0
cmanu07 <cemanuel86@gmail.com>
2022-09-09 18:56:11 +0300

    traffic light project update

--- src/components/projects/TrafficL.js
+++ src/components/projects/TrafficL.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../Footer'
 import Header from '../Header/Header'
 
 const TrafficL = () => {
+
+  const [redLight, setRedLight] = useState('red-light');
+  const [yellowLight, setYellowLight] = useState('yellow-light');
+  const [greenLight, setGreenLight] = useState('green-light');
+
+  const dayMode = () => {
+    setRedLight('red-light')
+    setYellowLight('off-light')
+    setGreenLight('off-light')
+  }
+  const nightMode = () => {
+    setYellowLight('yellow-light')
+    yellowLight === 'yellow-light' && 
+        setTimeout(()=>setYellowLight('off-light'),1000)
+        
+    setRedLight('off-light')
+    setGreenLight('off-light')
+  }
+  
+
   return (<>
             <Header/>
-            <div>TrafficL</div>
-            <div>TrafficL</div>
-            <div>TrafficL</div>
-            <div>TrafficL</div>
+            <h2>Traffic Light</h2>
+            <main>
+              <div className="semafor">
+                  <p className={redLight}></p>
+                  <p className={yellowLight}></p>
+                  <p className={greenLight}></p>
+              </div>
+              <div className="modul">
+                  <button className="but-day" onClick={dayMode}>DAY</button>
+                  <div className="stalp"></div>
+                  <button className="but-night" onClick={nightMode}>NIGHT</button>
+              </div>
+            </main>
             <Footer/>
         </>)
 }
