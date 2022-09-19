722e7c109cea4275328f8a9bc1e9a8500fded2f4
cmanu07 <cemanuel86@gmail.com>
2022-09-19 11:24:11 +0300

    content and more style updates

--- src/components/Pages/Despre.js
+++ src/components/Pages/Despre.js
@@ -6,10 +6,10 @@ export default function Despre () {
 
     return (<>
         <Header/>
-        <h2>Despre</h2>
+        <h2>ABOUT ME</h2>
         <div className="about-main">
             <p>I'm Emanuel and I graduated from Babes-Balyai University in 2008 with a degree in Finances. After 13+ years 
-                of experience in the field I'm looking to transition to IT work field as it has always been one of my 
+                of experience in the field I'm looking to transition to IT as it has always been one of my 
                 passions.</p>
             <p>I have just completed a certified course with Fasttrack IT Cluj-Napoca in Web Development and i'm eager to put my knowledge into practice.</p>
             <p>I am now looking for a Junior Web Developer position to finally kick start my career and learn among professionals.</p> 
