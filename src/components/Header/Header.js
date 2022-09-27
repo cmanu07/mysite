c3299734ed74cc76abd13169fefb2f4e042ac45d
cmanu07 <cemanuel86@gmail.com>
2022-09-27 20:15:49 +0300

    navbar updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import NavBar from "./NavBar"
 import { ThemeContext } from "../Contexts/ThemeContext";
 import myLogo from "../Media/my_logo.png"
+import ShowDate from "../Main/ShowDate";
 
 const Header = () => {
 
     const {toggleTheme, theme} = useContext(ThemeContext);
 
+    const [navbar, setNavbar] = useState(() => true)
+    const [toggleMenu, setToggleMenu] = useState(() => true)
+    const [toggleMenuP, setToggleMenuP] = useState(() => true)
+
     let className = "header";
     const navList = [{buton1: 'PROJECTS', buton2: 'CV', buton3: 'ABOUT ME'}];
+
+    const toggleMenuFunct = () => {
+        setToggleMenu(!toggleMenu);
+        setToggleMenuP(!toggleMenuP);
+        setNavbar(!navbar);
+    }
 
     return (
         <div className = {`${className}`}>
             <h1 title="Emanuel Caradan - My Site"><a href="/"><img src={myLogo} alt="my site logo"/>MANU</a></h1>
-            {navList.map((buton) => {
-                return <NavBar
+            <nav className={navbar ? "navbar-off" : "navbar"}>
+                {navList.map((buton) => {
+                    return <NavBar
                     key = {buton.buton1}
                     proiecte = {buton.buton1}
                     cv = {buton.buton2}
                     despre = {buton.buton3}
-                />
-            })}
+                    />
+                })}
+            </nav>
+            <ShowDate/>
             <label className="toggle-theme">
                 <input type='checkbox' onChange={toggleTheme} checked={theme === 'light'}/>
                 <span className="toggle-moon-icon"></span>
                 <span className="toggle-sun-icon"></span>
                 <span className="slider"></span>
             </label>
+            
+            <div className={toggleMenu ? "toggle" : "toggle-active"} onClick={toggleMenuFunct}>
+                <p className={toggleMenuP ? "toggle-p" : "toggle-p-off"}></p>
+            </div>
         </div>
     )
 };
---
+++ src/components/Main/ShowDate.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const ShowDate = () => {
+    
+  
+  const [clock, setClock] = useState(() => "");
+  const [date, setDate] =useState(() => "")
+  
+  useEffect(() => {
+    setInterval(() => {
+          const data = new Date();
+          let day = data.getDay();
+          switch (day) {
+              case 0 : day = "Sunday"; break;
+              case 1 : day = "Monday"; break;
+              case 2 : day = "Tuesday"; break;
+              case 3 : day = "Wednesday"; break;
+              case 4 : day = "Thursday"; break;
+              case 5 : day = "Friday"; break;
+              case 6 : day = "Saturday"; break;
+              default: day = "~unknown~"
+          }
+          setDate(day);
+          setClock(data.toLocaleTimeString());
+        }, 1000)
+    }, [])
+
+    
+  return (
+    <div className='show-date'>
+      <p>{date}</p>
+      <p>{clock}</p>
+    </div>
+  )
+}
+
+export default ShowDate
