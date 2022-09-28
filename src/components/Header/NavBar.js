a312f24bc34d1c3f9b67ec66b074ac6c22157bdd
cmanu07 <cemanuel86@gmail.com>
2022-09-28 11:50:05 +0300

    navbar updates

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,7 +1,7 @@
 
 const NavBar = (props) => {
 
-    return ( <div className="navbar">
+    return ( <div>
                 <h3><a href="/projects">{props.proiecte}</a></h3>
                 <h3><a href="/myCV">{props.cv}</a></h3>
                 <h3><a href="/about">{props.despre}</a></h3>
