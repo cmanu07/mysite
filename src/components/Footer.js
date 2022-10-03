5bfaf140bfc0ab35e163d12bd45c7db147c960b5
cmanu07 <cemanuel86@gmail.com>
2022-10-03 10:55:00 +0300

    minor updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,9 +29,15 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="footer-social">
-                        <a href="https://wa.me/40744301841" target="_blank" rel="noreferrer"><img src={whatsApp} alt="logo WhatsApp"/></a>
-                        <a href="https://www.github.com/cmanu07" target="_blank" rel="noreferrer"><img src={gitHub} alt="logo GitHub"/></a>
-                        <a href="https://www.linkedin.com/in/emanuel-caradan-7b12a3228" target="_blank" rel="noreferrer"><img src={linkedIn} alt="logo LinkedIn"/></a>
+                        <a href="https://wa.me/40744301841" target="_blank" rel="noreferrer">
+                            <img src={whatsApp} alt="logo WhatsApp"/>
+                        </a>
+                        <a href="https://www.github.com/cmanu07" target="_blank" rel="noreferrer">
+                            <img src={gitHub} alt="logo GitHub"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/emanuel-caradan-7b12a3228" target="_blank" rel="noreferrer">
+                            <img src={linkedIn} alt="logo LinkedIn"/>
+                        </a>
                     </div>
                     <h4>Copyright © Emanuel Caradan 2022</h4>
                 </article>
