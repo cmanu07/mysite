bbee2782d79fd21ba6c8a193fa551e1bf24b2587
cmanu07 <cemanuel86@gmail.com>
2023-08-14 11:04:02 +0300

    final touches

--- src/components/Pages/CVPage/CVPage.js
+++ src/components/Pages/CVPage/CVPage.js
@@ -3,7 +3,10 @@ import React from "react";
 import "./CVPage.css";
 
 import cvItem from "../../Media/cv_item.jpg";
-import cvFile from "../../Files/Emanuel Caradan - CV.pdf";
+import cvFile from "../../Files/Emanuel_Caradan_CV.pdf";
+import UiPathFoundation from "../../Files/LearningPath_Certificate_Foundation_Emanuel_Caradan.pdf"
+import UiPathAdvanced from "../../Files/LearningPath_Certificate_Advanced_Emanuel_Caradan.pdf"
+
 
 export default function CVPage () {
 
@@ -11,6 +14,7 @@ export default function CVPage () {
         <div className="cv">
             <h2>CV</h2>
             <img src={cvItem} alt="my updated cv"/>
+            <p>Here you can see my UiPath Diploma of completion: <a href={UiPathFoundation} download>Foundation</a> and <a href={UiPathAdvanced} download>Advanced</a>.</p>
             <p>You can also download my CV in PDF format from <a href={cvFile} download>here</a>.</p>
         </div>
         </>)
